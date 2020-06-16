1e71afb81948336219f1ff50134d01ea844368ea
Zakharov Roman <zakharovroman@hotmail.com>
2020-06-16 21:14:31 +0300

    Initial Commit

---
+++ HW2_9_Task_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2_9_Task_1
+//
+//  Created by Роман Захаров on 16.06.2020.
+//  Copyright © 2020 Роман Захаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
